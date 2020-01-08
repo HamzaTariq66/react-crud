7fee97eae84ffdd7496ac4b148c10ec69368633f
hami.cool15@gmail.com <hami.cool15@gmail.com>
2020-01-08 15:57:47 +0500

    first commit

---
+++ build/precache-manifest.e11e69fd9c9d954e6ae4c52af35878a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa31c8601a28eeec40d935b5d48a8bac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "733e4486aeeafca39a41",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0951840f22c68e3868ee",
+    "url": "/static/css/main.d9754bfc.chunk.css"
+  },
+  {
+    "revision": "733e4486aeeafca39a41",
+    "url": "/static/js/2.e5b7034d.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/static/js/2.e5b7034d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0951840f22c68e3868ee",
+    "url": "/static/js/main.395dc306.chunk.js"
+  },
+  {
+    "revision": "c9f83dd70aba6d3b6049",
+    "url": "/static/js/runtime-main.5738ede9.js"
+  }
+]);
